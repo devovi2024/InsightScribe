af0ee511f42935835270f10e24af30f98fa24503
devovi2024 <arfanhosenovi@gmail.com>
2025-03-23 01:39:25 +0600

    client

--- server/server.js
+++ server/server.js
@@ -6,14 +6,12 @@ dotenv.config();
 
 const app = express();
 
-// Define a route that handles GET requests
 app.get("/", (req, res) => {
   res.send("Inside Scribe , World!");
 });
 
 const PORT = process.env.PORT || 5000;
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
